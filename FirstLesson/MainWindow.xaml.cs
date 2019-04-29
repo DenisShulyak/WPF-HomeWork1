002c2993f94ea2e06d8c7651203bcd25b8a21420
Шуляк Денис Максимович <@corp.itstep>
2019-04-29 18:10:18 +0600

    HW1

---
+++ FirstLesson/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FirstLesson
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        Student student = new Student();
+        private void buttonCreate_Click(object sender, RoutedEventArgs e)
+        {
+             student = new Student
+            {
+                Name = textBoxName.Text,
+                Surname = textBoxSurname.Text,
+                YearBirth = int.Parse(textBoxYear.Text),
+                MonthBirth = int.Parse(textBoxMonth.Text),
+                DayBirth = int.Parse(textBoxDay.Text),
+                Course = int.Parse(textBoxCourse.Text),
+                Group = textBoxGroup.Text
+            };
+
+            
+        }
+
+        private void buttonShow_Click(object sender, RoutedEventArgs e)
+        {
+
+            MessageBox.Show("Студент: \n" +
+          "Имя: " + student.Name + "\n" +
+         "Дата рождения: " + student.DayBirth + "/" + student.MonthBirth + "/" + student.YearBirth + "\n" +
+         "Курс: " + student.Course + "\n" +
+          "Группа: " + student.Group);
+        }
+
+       
+    }
+}
---
+++ FirstLesson/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstLesson
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int YearBirth { get; set; }
+        public int MonthBirth { get; set; }
+        public int DayBirth { get; set; }
+        public int Course { get; set; }
+        public string Group { get; set; }
+    }
+}
